d86d27fadfcc128d11d0b3815e57c33533e37a8c
PraisesPJMT <praisesmusa@outlook.com>
2022-10-26 22:14:19 +0100

    Add method to decode `morse-code` word

--- morse_code.rb
+++ morse_code.rb
@@ -1,25 +1,34 @@
 MORSE_LETTERS = {
-'.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
-'.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
-'..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
-'--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
-'--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-'..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
-'-.--' => 'Y', '--..' => 'Z'
-}
-
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+  '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+  '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+  '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+  '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+  '-.--' => 'Y', '--..' => 'Z'
+}.freeze
 
 def decode_char(morse)
-MORSE_LETTERS[morse].upcase
+  MORSE_LETTERS[morse].upcase
 end
 
 def decode_word(word)
-characters = word.split
-characters.each_with_index do |char, index|
-characters[index] = decode_char(char)
-end
-characters.join
+  characters = word.split
+  characters.each_with_index do |char, index|
+    characters[index] = decode_char(char)
+  end
+  characters.join
 end
 
-puts decode_char('-.-' )
-puts decode_word("-- -.--")
+def decode(message)
+  words = message.split('   ')
+  words.each_with_index do |word, index|
+    words[index] = decode_word(word)
+  end
+  words.join(' ')
+end
+
+puts decode_char('-.-')
+puts decode_word('-- -.--')
+puts decode('-- -.--   -. .- -- .')
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
